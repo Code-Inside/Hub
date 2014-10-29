6fe83d6b93439eacadeec1faa8e52fd40a8378e6
Robert Muehsig <robert.muehsig@code-developer.de>
2014-10-29 23:42:29 +0100

    magic bootstrap and new logo

--- CodeInside.Hub/CodeInside.Hub.Web/App_Start/BundleConfig.cs
+++ CodeInside.Hub/CodeInside.Hub.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace CodeInside.Hub.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/metro-bootstrap.css",
+                      "~/Content/magic-bootstrap.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
