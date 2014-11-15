26408bdcc3735a47e09b9c0ae5dc9bfe7525140c
Robert Muehsig <robert.muehsig@code-developer.de>
2014-11-15 19:18:10 +0100

    style from live bootstrap

--- CodeInside.Hub/CodeInside.Hub.Web/App_Start/BundleConfig.cs
+++ CodeInside.Hub/CodeInside.Hub.Web/App_Start/BundleConfig.cs
@@ -25,7 +25,6 @@ namespace CodeInside.Hub.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
