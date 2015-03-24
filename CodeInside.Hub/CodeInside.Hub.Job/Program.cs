23f0c982c8ea662be1b53843eda0a89a7d53806e
Robert Muehsig <robert.muehsig@code-developer.de>
2015-03-24 23:24:46 +0100

    sloader integration

--- CodeInside.Hub/CodeInside.Hub.Job/Program.cs
+++ CodeInside.Hub/CodeInside.Hub.Job/Program.cs
@@ -10,6 +10,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sloader.Crawler.Config;
+using Constants = CodeInside.Hub.Domain.Constants;
+using MasterCrawlerConfig = CodeInside.Hub.Domain.MasterCrawlerConfig;
+using MasterCrawlerSecrets = CodeInside.Hub.Domain.MasterCrawlerSecrets;
 
 namespace CodeInside.Hub.Job
 {
@@ -19,24 +23,45 @@ namespace CodeInside.Hub.Job
         {
             Trace.TraceInformation("Crawler Console App started.");
 
-            var crawlerResult = InvokeCrawler().Result;
+            var crawlerResult = InvokeLegacyCrawler().Result;
             Trace.TraceInformation("Crawler succeeded - now convert and write to BlobStorage!");
 
             var json = JsonConvert.SerializeObject(crawlerResult, Constants.CrawlerJsonSerializerSettings);
 
             var host = new JobHost();
-            host.Call(typeof(Program).GetMethod("SaveToAzure"), new { json });
+            host.Call(typeof(Program).GetMethod("SaveToAzureLegacy"), new { json });
         }
 
         [NoAutomaticTrigger]
-        public static void SaveToAzure([Blob("hub/data.json")]TextWriter writer, string json)
+        public static void SaveToAzureLegacy([Blob("hub/data.json")]TextWriter writer, string json)
         {
             writer.Write(json);
 
             Trace.TraceInformation("And... done.");
         }
 
-        public static async Task<CrawlerRun> InvokeCrawler()
+        [NoAutomaticTrigger]
+        public static void SaveToAzure([Blob(Sloader.Crawler.Config.Constants.SloaderAzureBlobContainer + "/" + Sloader.Crawler.Config.Constants.SloaderAzureBlobFileName)]TextWriter writer, string json)
+        {
+            writer.Write(json);
+
+            Trace.TraceInformation("And... done.");
+        }
+
+        public static async Task<Sloader.Results.CrawlerRun> InvokeCrawler()
+        {
+            var config = await Sloader.Crawler.Config.MasterCrawlerConfig.Load(
+                    "https://raw.githubusercontent.com/Code-Inside/Hub/master/CodeInside.Hub/CodeInside.Hub.Web/App_Data/Sloader.yaml");
+
+            var secrets = new Sloader.Crawler.Config.MasterCrawlerSecrets();
+            secrets.TwitterConsumerKey = ConfigurationManager.AppSettings[ConfigKeys.SecretTwitterConsumerKey];
+            secrets.TwitterConsumerSecret = ConfigurationManager.AppSettings[ConfigKeys.SecretTwitterConsumerSecret];
+            var crawler = new Sloader.Crawler.MasterCrawler(config, secrets);
+
+            return await crawler.RunAllCrawlers();
+        }
+
+        public static async Task<CrawlerRun> InvokeLegacyCrawler()
         {
             var client = new HttpClient();
 #if DEBUG
